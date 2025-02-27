9a269e7f33a7c6f552c7c6354c58a6a30937c8b7
liufayi <314817473@qq.com>
2025-02-27 17:18:57 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ addEventListener("fetch", (event) => {
 });
 
 const dockerHub = "https://registry-1.docker.io";
-
+const CUSTOM_DOMAIN = "99.com"
 const routes = {
   // production
   ["docker." + CUSTOM_DOMAIN]: dockerHub,
